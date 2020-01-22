28443b5e901d192f0d229b1a9c4e7f5e951fcadb
Pedro Kretikouski Roque Junior <pedro.junior@ringa.com.br>
2020-01-22 11:44:48 -0300

    Cria as Rotas e adiciona ao componente Raiz

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View style={ styles.container }>
-      <Text style={ styles.title }> Hellow React Native </Text>
-    </View>
-  );
+    <>
+      <Routes />
+    </>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    fontWeight: 'bold',
-    fontSize: 32,
-    color: '#FFF'
-  }
-});
